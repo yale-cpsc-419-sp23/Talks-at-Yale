0d6c99e31a70b7569a02150a36a8c983a33003df
winiboya <w>
2023-04-12 09:57:02 -0400

    modal update

--- talksatyale/src/app/eventModal.js
+++ talksatyale/src/app/eventModal.js
@@ -14,41 +14,45 @@ export default function EventModal({ event, onClose }) {
         <div className={styles.modalContent}>
             <div className={styles.modalHeader}>
                 <FaRegBookmark size={40} className={styles.modalBookmark}/>
-                <div className={styles.modalDetails}>
-                    <h2 className={styles.modalTitle}>{event.title}</h2>
-                </div>
+                <h2 className={styles.modalTitle}>{event.title}</h2>
                 <div className={styles.modalClose} onClick={onClose}>
                     <FaTimes />
                 </div>
 
             </div>
             <div className={styles.modalSubHeader}>
-                <h3>{event.department}</h3>
-                <h3><a href="https://calendar.google.com" className={styles.addCalendar}>Add to Google Calendar</a></h3>
+                <h4 className={styles.modalDept}>{event.department}</h4>
+                <h4 className={styles.addCalendar}>|</h4>
+                <h4><a href="https://calendar.google.com" className={styles.addCalendar}> add to Google Calendar</a></h4>
 
             </div>
+            <hr className={styles.hLine}></hr>
 
             <div className={styles.modalBody}>
-                {event.description}
+                <p className={styles.modalDesc}>{event.description}</p>
             </div>
-            <div>
-                {event.bio}
+
+            <div className={styles.modalDetails}>
+                <div className={styles.modalRow}>
+                    <h4 className={styles.modalLabel}>Date</h4>
+                    <h4 className={styles.modalDetail}>{event.date}</h4>
+                </div>
+                <div className={styles.modalRow}>
+                    <h4 className={styles.modalLabel}>Time</h4>
+                    <h4 className={styles.modalDetail}>{event.time}</h4>
+                </div>
+                <div className={styles.modalRow}>
+                    <h4 className={styles.modalLabel}>Speaker</h4>
+                    <h4 className={styles.modalDetail}>{event.speaker}</h4>
+                </div>
+                <div className={styles.modalRow}>
+                    <h4 className={styles.modalLabel}>Location</h4>
+                    <h4 className={styles.modalDetail}>{event.location}</h4>
+                </div>
             </div>
-            <div>
-                {event.speaker}
-            </div>
-            <div>
-                {event.speaker_title}
-            </div>
-            <div>
-                {event.location}
-            </div>
-            <div>
-                {event.date}
-            </div>
-            <div>
-                {event.time}
-            </div>
+            <hr className={styles.hLine}></hr>
+            <h4 className={styles.modalFriends}>Inssia, Martin, Alan, and others have saved this event.</h4>
+
 
         </div>
     </div>
