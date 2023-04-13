b427f7475b2a745cfeeea09db664380b21d2ae8c
Abuu254 <abubakarramadhan254@gmail.com>
2023-04-13 19:20:12 -0400

    Added getiso

--- flask-api/scraping/DateTime.py
+++ flask-api/scraping/DateTime.py
@@ -1,5 +1,6 @@
 """Helper functions to be used in formating the data scrapped from departments"""
 from datetime import datetime
+from dateutil import parser
 
 ###-------------- Functions ------------------------------###
 
@@ -19,3 +20,28 @@ def getTime(str):
     hour = date_time_obj.hour
     minute = date_time_obj.minute
     return date_time_obj.strftime("%H:%M")
+
+import datetime
+
+def getISO(date_str):
+    """Convert date to ISO format"""
+    # parse the input date string using dateutil
+    date = parser.parse(date_str)
+    # format the datetime object in ISO format
+    iso_date = date.strftime("%Y-%m-%dT%H:%M:%S")
+    return iso_date
+
+# input date string in various formats
+date_str1 = "Monday, June 28 2005"
+date_str2 = "April 4 2024"
+date_str3 = "28/05/2023"
+
+# call the function to get the date in ISO format
+result1 = getISO(date_str1)
+result2 = getISO(date_str2)
+result3 = getISO(date_str3)
+
+# output the ISO-formatted dates
+print(result1)
+print(result2)
+print(result3)
