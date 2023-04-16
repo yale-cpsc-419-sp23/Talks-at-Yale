10dc0c410d9ab5c28408a87264eda3be7bd2a558
winiboya <w>
2023-04-16 05:48:02 -0400

    had to switch back lol

--- talksatyale/src/app/page.js
+++ talksatyale/src/app/page.js
@@ -51,85 +51,85 @@ export default function Home() {
 
 
 
- 
+     // get search results from search term, passed from child (header)
+     const [searchResults, setSearchResults] = useState([]);
+     const handleSearchResults = (results) => {
+       console.log(results);
+       setSearchResults(results);
+     }
+   
+     // get departments from backend
+     const [depts, setDepts] = useState([]);
+   
+     const [favoriteEventIDs, setFavoriteEventIDs] = useState([]);
+   
+   
+     const[data, setData] = useState([{}])
+   
+     useEffect(() => {
+   
+       async function fetchDepts() {
+         console.log('Fetching departments')
+         const response = await fetch(`http://localhost:8080/events/departments`).then(
+           res => res.json()
+         ).then(
+           data => {
+             setDepts(data);
+             console.log('Dept data:', data);
+           }
+         );
+   
+       }
+       fetchDepts();
+     }, []);
+   
+     // Getfavorited ids
+   
+     useEffect(() => {
+       const fetchFavoriteEventIDs = async () => {
+         try {
+           const response = await fetch(`http://localhost:8080/events/events_status`, {
+             headers: {
+               Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+             },
+           });
+           const data = await response.json();
+           setFavoriteEventIDs(data.IDS);
+         } catch (error) {
+           console.error('Error fetching favorite event IDs:', error);
+         }
+       };
+   
+       fetchFavoriteEventIDs();
+     }, []);
+   
+   
+   
+     // calls the function to get events
+     useEffect(() => {
+       const accessToken = localStorage.getItem('access_token');
+       const headers = new Headers();
+       if (accessToken) {
+         headers.append('Authorization', `Bearer ${accessToken}`);
+       }
+       async function fetchResults() {
+         const response = await fetch(`http://localhost:8080/events/search`,
+         {headers: headers,}
+         ).then(
+           res => res.json()
+         ).then(
+           data => {
+             setSearchResults(data);
+           }
+         );
+   
+       }
+       fetchResults();
+     }, []);
 
   if(loggedIn) {
 
-     // get search results from search term, passed from child (header)
-  const [searchResults, setSearchResults] = useState([]);
-  const handleSearchResults = (results) => {
-    console.log(results);
-    setSearchResults(results);
-  }
 
-  // get departments from backend
-  const [depts, setDepts] = useState([]);
-
-  const [favoriteEventIDs, setFavoriteEventIDs] = useState([]);
-
-
-  const[data, setData] = useState([{}])
-
-  useEffect(() => {
-
-    async function fetchDepts() {
-      console.log('Fetching departments')
-      const response = await fetch(`http://localhost:8080/events/departments`).then(
-        res => res.json()
-      ).then(
-        data => {
-          setDepts(data);
-          console.log('Dept data:', data);
-        }
-      );
-
-    }
-    fetchDepts();
-  }, []);
-
-  // Getfavorited ids
-
-  useEffect(() => {
-    const fetchFavoriteEventIDs = async () => {
-      try {
-        const response = await fetch(`http://localhost:8080/events/events_status`, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-          },
-        });
-        const data = await response.json();
-        setFavoriteEventIDs(data.IDS);
-      } catch (error) {
-        console.error('Error fetching favorite event IDs:', error);
-      }
-    };
-
-    fetchFavoriteEventIDs();
-  }, []);
-
-
-
-  // calls the function to get events
-  useEffect(() => {
-    const accessToken = localStorage.getItem('access_token');
-    const headers = new Headers();
-    if (accessToken) {
-      headers.append('Authorization', `Bearer ${accessToken}`);
-    }
-    async function fetchResults() {
-      const response = await fetch(`http://localhost:8080/events/search`,
-      {headers: headers,}
-      ).then(
-        res => res.json()
-      ).then(
-        data => {
-          setSearchResults(data);
-        }
-      );
-
-    }
-    fetchResults();
-  }, []);
     return (
     
       <div className={styles.pageWrapper}>
